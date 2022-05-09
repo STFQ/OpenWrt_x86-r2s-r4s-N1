67a2918ab2472290135e86ac94ac41920bb7e1ed
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-05-09 15:03:16 +0800

    update

--- devices/mediatek_mt7622/diy.sh
+++ devices/mediatek_mt7622/diy.sh
@@ -4,8 +4,6 @@ shopt -s extglob
 
 curl -sfL https://raw.githubusercontent.com/x-wrt/x-wrt/master/target/linux/mediatek/patches-5.15/995-0001-hwnat-add-natflow-flow-offload-support.patch -o target/linux/mediatek/patches-5.15/995-0001-hwnat-add-natflow-flow-offload-support.patch
 
-svn export --force https://github.com/x-wrt/x-wrt/trunk/package/kernel/mt76 package/kernel/mt76
-
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += my-autocore-arm luci-app-cpufreq luci-app-natflow-users natflow-boot/' target/linux/mediatek/Makefile
 
 echo '
--- devices/ramips_mt7621/diy.sh
+++ devices/ramips_mt7621/diy.sh
@@ -4,7 +4,4 @@ shopt -s extglob
 
 curl -sfL https://raw.githubusercontent.com/x-wrt/x-wrt/master/target/linux/mediatek/patches-5.15/995-0001-hwnat-add-natflow-flow-offload-support.patch -o target/linux/ramips/patches-5.15/995-0001-hwnat-add-natflow-flow-offload-support.patch
 
-svn export --force https://github.com/x-wrt/x-wrt/trunk/package/kernel/mt76 package/kernel/mt76
-
-
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += luci-app-natflow-users natflow-boot/' target/linux/ramips/Makefile
